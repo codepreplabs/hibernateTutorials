1a13806cfa5bd1fdff1b9cafd869f91757de6960
thejavamonk <thejavamonk@gmail.com>
2015-05-26 16:09:41 +0530

    example of fetchtype attribute of @ElementCollection annotation

---
+++ 14fetchtype/src/main/java/com/codeprep/vo/AddressVO.java
@@ -0,0 +1,45 @@
+package com.codeprep.vo;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class AddressVO {
+
+	private String addressLine;
+	private String city;
+	private String state;
+	private String country;
+
+	public String getAddressLine() {
+		return addressLine;
+	}
+
+	public void setAddressLine(String addressLine) {
+		this.addressLine = addressLine;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+}
---
+++ 14fetchtype/src/main/java/com/codepreplabs/main/TestHibernate.java
@@ -0,0 +1,53 @@
+package com.codepreplabs.main;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.codeprep.vo.AddressVO;
+import com.codepreplabs.entity.Employee;
+
+public class TestHibernate {
+
+	public static void main(String[] args) {
+
+		Employee employee = new Employee();
+		employee.setFirstName("Harry");
+		employee.setLastname("Potter");
+		employee.setDateOfBirth(new Date());
+
+		AddressVO permanentAddressVO = new AddressVO();
+		permanentAddressVO.setAddressLine("Privet Drive, Little Whinging,");
+		permanentAddressVO.setCity("Surrey");
+		permanentAddressVO.setState("England");
+		permanentAddressVO.setCountry("United Kingdom");
+
+		AddressVO temporaryAddressVO = new AddressVO();
+
+		temporaryAddressVO
+				.setAddressLine("Hogwarts School of Witchcraft and Wizardry");
+		temporaryAddressVO.setState("Scotland");
+		temporaryAddressVO.setCountry("United Kingdom");
+		
+		employee.getAddresses().add(temporaryAddressVO);
+		employee.getAddresses().add(permanentAddressVO);
+
+		Configuration configuration = new Configuration().configure();
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(employee);
+		session.getTransaction().commit();
+		session.close();
+		
+		Session session2 = sessionFactory.openSession();
+		Employee employee2 = (Employee) session2.get(Employee.class, 1);
+		session2.close();
+		
+		System.out.println(employee2.getAddresses().get(1).getAddressLine());
+
+	}
+
+}
