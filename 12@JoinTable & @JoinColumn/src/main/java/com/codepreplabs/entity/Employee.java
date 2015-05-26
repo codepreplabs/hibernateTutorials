675a95a58d8f49140d09941f2d4b2b4ece469804
thejavamonk <thejavamonk@gmail.com>
2015-05-26 13:48:18 +0530

    example for use of @JoinTable and @JoinColumn annotation

---
+++ 12@JoinTable & @JoinColumn/src/main/java/com/codepreplabs/entity/Employee.java
@@ -0,0 +1,75 @@
+package com.codepreplabs.entity;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.codeprep.vo.AddressVO;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int employeeId;
+	private String firstName;
+	private String lastname;
+
+	@Temporal(TemporalType.TIME)
+	private Date dateOfBirth;
+
+	@ElementCollection
+	@JoinTable(name="USER_ADDRESS", joinColumns= @JoinColumn(name="USER_ID"))
+	private List<AddressVO> Addresses = new ArrayList<AddressVO>();
+
+	public List<AddressVO> getAddresses() {
+		return Addresses;
+	}
+
+	public void setAddresses(List<AddressVO> addresses) {
+		Addresses = addresses;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(java.util.Date date) {
+		this.dateOfBirth = date;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+}
