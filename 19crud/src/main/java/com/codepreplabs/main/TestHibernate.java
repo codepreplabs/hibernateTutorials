d59c79c9894b204b01102d2466770dff13d5a962
praveen <thejavamonk@gmail.com>
2020-11-03 21:39:12 +0530

    updated example on CRUD methods of hibernate session object

--- 19crud/src/main/java/com/codepreplabs/main/TestHibernate.java
+++ 19crud/src/main/java/com/codepreplabs/main/TestHibernate.java
@@ -10,35 +10,37 @@ public class TestHibernate {
 
 	public static void main(String[] args) {
 
-		Employee employee = new Employee();
-		employee.setFirstName("Harry");
-		employee.setLastname("Potter");
-
-		Employee employee1 = new Employee();
-		employee1.setFirstName("Robin");
-		employee1.setLastname("hood");
-
 		Configuration configuration = new Configuration().configure();
 		SessionFactory sessionFactory = configuration.buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
+		
+		Employee employee1 = new Employee();
+		employee1.setFirstName("Harry");
+		employee1.setLastname("Potter");
+
+		Employee employee2 = new Employee();
+		employee2.setFirstName("Robin");
+		employee2.setLastname("hood");
 
 		
-/*		 // Saving an object using the save() method 
-		 session.save(employee);
-		 session.save(employee1);*/
+		 // Saving an object using the save() method 
+		 session.save(employee1);
+		 session.save(employee2);
 		 
 
-/*		// Retrieving object using get() method
-		Employee employee2 = (Employee) session.get(Employee.class, 1);*/
+		// Retrieving object using get() method
+//		Employee employee3 = (Employee) session.get(Employee.class, 1);
+//		System.out.println(employee3);
 		
-/*		// Deleting rows using session.delete() method	
-		employee1.setEmployeeId(1);
-		session.delete(employee1);*/
+		// Deleting rows using session.delete() method	
+//		employee1.setEmployeeId(1);
+//		session.delete(employee1);
 		
-/*		//updating rows using update or saveOrUpdate()
-		employee.setEmployeeId(2);
-		session.update(employee);*/
+		//updating rows using update or saveOrUpdate()
+//		employee2.setEmployeeId(2);
+//		employee2.setLastname("Updated");
+//		session.update(employee2);
 		
 		
 		session.getTransaction().commit();
