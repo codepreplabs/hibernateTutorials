86c7b25d2ecdbfedff8f80bd2be072b5c767da2d
praveen <thejavamonk@gmail.com>
2020-11-04 15:16:26 +0530

    example on positional and named parameters

--- 22parameterBinding/src/main/java/com/codepreplabs/main/SetupTables.java
+++ 22parameterBinding/src/main/java/com/codepreplabs/main/SetupTables.java
@@ -13,49 +13,49 @@ import com.codepreplabs.entity.Employee;
 public class SetupTables {
 
 	public static void main(String[] args) {
-		
+
 		List<Employee> employees = new ArrayList<Employee>();
 
 		Employee employee1 = new Employee();
 		employee1.setFirstName("Jhon");
 		employee1.setLastname("Doe");
-		
+
 		Employee employee2 = new Employee();
 		employee2.setFirstName("Jessica");
 		employee2.setLastname("Shaw");
-		
+
 		Employee employee3 = new Employee();
 		employee3.setFirstName("Jhon");
 		employee3.setLastname("Deere");
-		
+
 		Employee employee4 = new Employee();
 		employee4.setFirstName("Dheeraj");
 		employee4.setLastname("Shaw");
-		
+
 		Employee employee5 = new Employee();
 		employee5.setFirstName("Jessica");
 		employee5.setLastname("Simpson");
-		
+
 		Employee employee6 = new Employee();
 		employee6.setFirstName("Jasmine");
 		employee6.setLastname("Lal");
-		
+
 		Employee employee7 = new Employee();
 		employee7.setFirstName("Aicole");
 		employee7.setLastname("Anistone");
-		
+
 		Employee employee8 = new Employee();
 		employee8.setFirstName("Jessica");
 		employee8.setLastname("Balmer");
-		
+
 		Employee employee9 = new Employee();
 		employee9.setFirstName("Jhon");
 		employee9.setLastname("Snow");
-		
+
 		Employee employee10 = new Employee();
 		employee10.setFirstName("Jim");
 		employee10.setLastname("Henry");
-		
+
 		employees.add(employee1);
 		employees.add(employee2);
 		employees.add(employee3);
@@ -66,13 +66,13 @@ public class SetupTables {
 		employees.add(employee8);
 		employees.add(employee9);
 		employees.add(employee10);
-		
+
 		Department department1 = new Department();
 		department1.setDepartmentName("Computer Science");
-		
+
 		Department department2 = new Department();
 		department2.setDepartmentName("Public Relations");
-		
+
 		employee1.setDepartment(department1);
 		employee2.setDepartment(department2);
 		employee3.setDepartment(department1);
@@ -83,7 +83,7 @@ public class SetupTables {
 		employee8.setDepartment(department2);
 		employee9.setDepartment(department1);
 		employee10.setDepartment(department2);
-		
+
 		department1.setEmployees(employees);
 		department2.setEmployees(employees);
 
@@ -103,7 +103,6 @@ public class SetupTables {
 		session.save(employee10);
 		session.getTransaction().commit();
 		session.close();
-		
 
 	}
 
--- 22parameterBinding/src/main/java/com/codepreplabs/main/TestHibernate.java
+++ 22parameterBinding/src/main/java/com/codepreplabs/main/TestHibernate.java
@@ -2,7 +2,7 @@ package com.codepreplabs.main;
 
 import java.util.List;
 
-import org.hibernate.Query;
+import org.hibernate.query.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
@@ -11,45 +11,45 @@ import com.codepreplabs.util.HibernateUtil;
 public class TestHibernate {
 
 	public static void main(String[] args) {
-		
-		String firstName ="Jessica";
-/*		try {
 
-			SessionFactory sessionFactory = HibernateUtil.getSessionfactory();
-			Session session = sessionFactory.openSession();
+		String firstName = "Jessica";
+
+		SessionFactory sessionFactory = HibernateUtil.getSessionfactory();
+		
+		
+		try (Session session = sessionFactory.openSession()) {
+
 			session.beginTransaction();
-			Query query = session.createQuery("SELECT firstName FROM Employee WHERE firstName = ?");
-			
-			query.setParameter(0, firstName);
+			Query query = session.createQuery("SELECT firstName FROM Employee WHERE firstName = ?1");
+
+			query.setParameter(1, firstName);
 
 			List<String> firstNames = query.list();
 			for (String firstname : firstNames)
 				System.out.println(firstname);
 
 			session.getTransaction().commit();
-			session.close();
-		} catch (Throwable e) {
-			System.out.println(e.getMessage());
-		}*/
-		
-		try {
-
-			SessionFactory sessionFactory = HibernateUtil.getSessionfactory();
-			Session session = sessionFactory.openSession();
-			session.beginTransaction();
-			Query query = session.createQuery("SELECT firstName FROM Employee WHERE firstName = :firstName");
-			
-			query.setString("firstName", firstName);
-
-			List<String> firstNames = query.list();
-			for (String firstname : firstNames)
-				System.out.println(firstname);
-
-			session.getTransaction().commit();
-			session.close();
-		} catch (Throwable e) {
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+
+		try (Session session = sessionFactory.openSession()){
+
+			session.beginTransaction();
+			Query<String> query = session.createQuery("SELECT firstName FROM Employee WHERE firstName = :firstName",
+					String.class);
+
+			query.setParameter("firstName", firstName);
+
+			List<String> firstNames = query.getResultList();
+			for (String firstname : firstNames)
+				System.out.println(firstname);
+
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
 	}
 
 }
